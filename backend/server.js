dc3cb426baeb490c71b678cabaf8fcb1422b0b22
KHORSI Yacine <yacinekhorsi1@gmail.com>
2022-08-21 17:46:11 +0200

    ok

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,9 @@
 // console.log("voila du code javascriptnode ");
 const http = require("http");
+const { env } = require("process");
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  res.end("voila la reponse du serveur");
+});
+
+server.listen(process.env.PORT || 3000);
